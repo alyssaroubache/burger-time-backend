f8a99214b9b06143476fe2f039135f1d8c1e58b1
alyssaroubache <alyssaroubache1124@gmail.com>
2026-05-21 04:55:51 +0200

    force ipv4 smtp

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT),
     secure:true,
+    family: 4,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
