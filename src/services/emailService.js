3b298c327ebe25b738315b9079b4ac50f21854e7
alyssaroubache <alyssaroubache1124@gmail.com>
2026-05-21 05:06:00 +0200

    debug smtp env

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -2,6 +2,11 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+console.log("SMTP_HOST =", process.env.SMTP_HOST);
+console.log("SMTP_PORT =", process.env.SMTP_PORT);
+console.log("SMTP_USER =", process.env.SMTP_USER);
+console.log("SMTP_SECURE =", process.env.SMTP_SECURE);
+
 const transporter = nodemailer.createTransport({
     host:'74.125.140.108',
     port: parseInt(process.env.SMTP_PORT),
